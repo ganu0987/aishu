65bb562b394afc55d0d5373c2eee086a06f73367
ganu0987 <ganavihs649@gmail.com>
2025-07-03 03:14:38 -0400

    Add files via upload

---
+++ class date.py
@@ -0,0 +1,10 @@
+class Date:
+    def__ init__(self,a,b,c):
+        self.d=a
+        self.m=b
+        self.y=c
+        def day(self):
+            print("day=",self.d)
+            def month(self):
+                print("month=",self.m)
+                def
